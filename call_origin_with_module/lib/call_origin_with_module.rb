c7bd49e566cd1fb7578d01a97d67d99e0cfa3b0e
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-06-04 14:22:16 +0200

    reproduce the all orginal issue

--- call_origin_with_module/lib/call_origin_with_module.rb
+++ call_origin_with_module/lib/call_origin_with_module.rb
@@ -1,5 +1,14 @@
 require "call_origin_with_module/version"
+require "jrjackson"
 
-module CallOriginWithModule
-  # Your code goes here...
+module AClass
+
+  extend self
+  def bar(n)
+    JrJackson::Raw.generate({:source => 3, :target => n})
+  end
+
+  def nap(n)
+    JrJackson::Json.dump({:source => 3, :target => n})
+  end
 end
--- call_origin_with_module/spec/call_origin_with_module_spec.rb
+++ call_origin_with_module/spec/call_origin_with_module_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
-describe CallOriginWithModule do
-  it 'has a version number' do
-    expect(CallOriginWithModule::VERSION).not_to be nil
+describe "CallOriginWithModule" do
+
+  it "calls the generate once" do
+    expect(JrJackson::Raw).to receive(:generate).with({:source=>3, :target=>-3}).and_call_original
+    expect(AClass.bar(-3)).to eq("{\"source\":3,\"target\":-3}")
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it "calls the generate twice" do
+    expect(JrJackson::Raw).to receive(:generate).with({:source=>3, :target=>3}).and_call_original
+    expect(AClass.bar(3)).to eq("{\"source\":3,\"target\":3}")
   end
+
+   it "calls the dump once" do
+    expect(JrJackson::Raw).to receive(:dump).with([3,-3]).and_call_original
+    expect(AClass.nap(-3)).to eq("[3,-3]")
+  end
+
+  it "calls the dump twice" do
+    expect(JrJackson::Raw).to receive(:dump).with([3,3]).and_call_original
+    expect(AClass.nap(3)).to eq("[3,3]")
+  end
+
+  describe "without using call original" do
+  
+    it "calls the generate once " do
+      expect(JrJackson::Raw).to receive(:generate).and_return("foo")
+      expect(AClass.bar(-3)).to eq("foo")
+    end
+
+    it "calls the generate twice" do
+      expect(JrJackson::Raw).to receive(:generate).and_return("oof")
+      expect(AClass.bar(3)).to eq("oof")
+    end
+
+  end
+
+
+
 end
